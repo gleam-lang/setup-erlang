871c4027e63720b371d6475792ac7b2803f12b76
Regan Heath <reganmheath@gmail.com>
2020-05-22 01:04:40 +0100

    Correct template string backticks

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const path = require("path");
 let main;
 if (process.platform === "win32") {
     let scriptfile = path.join(__dirname, "main.ps1")
-    main = "C:\\Program Files\\PowerShell\\7\\pwsh.EXE -command \". '${scriptfile}'\"";
+    main = `"C:\\Program Files\\PowerShell\\7\\pwsh.EXE" -command ". '${scriptfile}'"`;
 }
 else {
     main = path.join(__dirname, "main.sh")
